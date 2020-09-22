72e19860a18b2f08335be7b57e08d96754533b08
18011905 <18011905@d020m07.istic.univ-rennes1.fr>
2020-09-22 09:56:58 +0200

    asd impl

--- TP1/src/main/java/TP1/ASD.java
+++ TP1/src/main/java/TP1/ASD.java
@@ -3,11 +3,67 @@ package TP1;
 import java.util.List;
 
 public class ASD {
-  static public class Document {
-    // Fill here!
-    public String toNtriples() {
-      return "…";
-    }
-  }
-  // Fill here!
+	static public class Document {
+		
+		class Turtle {
+			List<Sujet> sujets;
+
+			public Turtle(List<Sujet> s) {
+				sujets = s;
+			}
+		}
+
+		class Sujet {
+			Nom nom;
+			List<Predicat> preds;
+
+			public Sujet(Nom n, List<Predicat> p) {
+				nom = n;
+				preds = p;
+			}
+		}
+
+		class Predicat {
+			Nom nom;
+			List<Objet> objets;
+
+			public Predicat(Nom n, List<Objet> o) {
+				nom = n;
+				objets = o;
+			}
+		}
+
+		abstract class Objet {
+
+		}
+
+		class ObjNom extends Objet {
+			Nom nom;
+
+			public ObjNom(Nom n) {
+				nom = n;
+			}
+		}
+
+		class ObjTxt extends Objet {
+			String string;
+
+			public ObjTxt(String s) {
+				string = s;
+			}
+		}
+
+		class Nom {
+			String string;
+
+			public Nom(String s) {
+				string = s;
+			}
+		}
+
+		public String toNtriples() {
+			return "…";
+		}
+	}
+
 }
